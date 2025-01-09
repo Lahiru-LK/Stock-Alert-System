db53e39c9744d7f76353f0d135ebba5182b3de49
Lahiru Prasad <104630433+Lahiru-LK@users.noreply.github.com>
2025-01-10 00:19:21 +0530

    SMS update sid and token fake. next update WhatsApp API connet and SMS

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+ACCOUNT_SID = "your_account_sid"
+AUTH_TOKEN = "your_auth_token"
+TWILIO_PHONE_NUMBER = "+your_twilio_phone_number"
+YOUR_PHONE_NUMBER = "+your_verified_phone_number"
+
+EXCEL_FILE_PATH = "stocks.xlsx"  # Replace with your Excel file path
